13094faead6743d0db35e7dfc0b2493d8be2793d
ruslanboleev <ruslanboleev007@gmail.com>
2025-02-18 21:41:17 +0700

    two

--- src/TaskTwo.java
+++ src/TaskTwo.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class TaskTwo {
     public static void main(String[] args) {
         //:Todo: Вам предоставляется массив элементов, и ваша задача - определить, есть ли
@@ -14,14 +15,24 @@ public class TaskTwo {
 
         int[] array = new int[length];
 
-        System.out.println("Введите элементы массива:");
+        System.out.print("Введите элементы массива:");
         for (int i = 0; i < length; i++) {
             array[i] = scanner.nextInt();
 
         }
 
-        System.out.println("Массив: " + Arrays.toString(array));
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] == array[j]) {
 
+                    System.out.println("Повторяющийся элемент: " + array[i]);
+                }
+            }
+        }
+
+
+
+        System.out.println("Массив: " + Arrays.toString(array));
     }
 }
 
